d28d786872798b1531078a3a5a585bc7be50f41c
MiuHataya <fyp.storygeneration@gmail.com>
2025-03-20 22:18:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ index.add(doc_embeddings)
 
 
 # ユーザーの質問を受け取る
-async def process_query(query, TARGET_SIMILARITY, SIMILARITY_THRESHOLD):
+def process_query(query, TARGET_SIMILARITY, SIMILARITY_THRESHOLD):
     query_embedding = embedding_model.encode([query])
     # FAISS を使って類似文書を検索 (上位5件)
     D, I = index.search(query_embedding, k=5)
@@ -77,14 +77,16 @@ async def process_query(query, TARGET_SIMILARITY, SIMILARITY_THRESHOLD):
 
     # Switch はここで
     if not summaries:
-      print("該当なし (新しい Summary を生成します)")
-      ai_answer = await generate_story(query)
+        print("該当なし (新しい Summary を生成します)")
+        ai_answer = asyncio.run(generate_story(query))
     else:
         print("\n 近似 5 件の類似 Summary を元に新しい Summary を生成しました")
         T5_answer = generate_summary_from_multiple_docs(summaries)
+        '''
         print("\n T5 が生成した Summary:")
         print(T5_answer)
-        ai_answer = await refine_summary_with_openai(T5_answer)
+        '''
+        ai_answer = asyncio.run(refine_summary_with_openai(T5_answer))
 
     return ai_answer
     '''
