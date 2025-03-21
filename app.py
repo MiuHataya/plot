8f3d7885352c00d90714eadd5ee08046de6db0c0
MiuHataya <fyp.storygeneration@gmail.com>
2025-03-21 21:59:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -140,8 +140,8 @@ def process_query(query, TARGET_SIMILARITY, SIMILARITY_THRESHOLD):
     # Switch はここで
     if not summaries:
         print("該当なし (新しい Summary を生成します)")
-        ai_answer = asyncio.run(generate_story(query))
-        #return jsonify({"error": "ナッシング！！"})
+        #ai_answer = asyncio.run(generate_story(query))
+        return jsonify({"error": "ナッシング！！"})
     else:
         #T5_answer = generate_summary_from_multiple_docs(summaries)
         print("\n 近似 5 件の類似 Summary を元に新しい Summary を生成しました")
@@ -149,8 +149,8 @@ def process_query(query, TARGET_SIMILARITY, SIMILARITY_THRESHOLD):
         #print("\n T5 が生成した Summary:")
         #print(T5_answer)
         '''
-        ai_answer = asyncio.run(refine_summary_with_openai(T5_answer))
-        #return jsonify({"error": "ありりんご！"})
+        #ai_answer = asyncio.run(refine_summary_with_openai(T5_answer))
+        return jsonify({"error": "ありりんご！"})
 
 
 @app.route("/", methods=["GET"])
