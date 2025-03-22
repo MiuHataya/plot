6dd8098e500a6b54ccff8088f38277708546c3b0
MiuHataya <fyp.storygeneration@gmail.com>
2025-03-22 09:33:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -58,6 +58,7 @@ def generate_summary_from_multiple_docs(input_doc, prefix="create a coherent sto
     combined_text = " ".join(input_doc)
     input_text = prefix + combined_text
     inputs = tokenizer_t5(input_text, return_tensors="pt", padding=True, truncation=True, max_length=256)
+    print(inputs)
     with torch.no_grad():
         output_ids = model_t5.generate(
             **inputs,
@@ -68,7 +69,7 @@ def generate_summary_from_multiple_docs(input_doc, prefix="create a coherent sto
             early_stopping=False
         )
     T5_gene = tokenizer_t5.decode(output_ids[0], skip_special_tokens=True)
-    print(T5_gene)
+    
 
 
 # ユーザーの質問を受け取る
